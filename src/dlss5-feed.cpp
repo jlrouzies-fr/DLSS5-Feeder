9dcb8c9a996b9c2b9edb0e0ef9c933a632a50dc2
Jean-Laurent ROUZIES <rouzies.jeanlaurent@gmail.com>
2026-09-24 18:30:01 +0200

    Prepare 1.17.0 beta 2
    Bump FEED_VERSION in both add-ons and src/version.rc together.

--- src/dlss5-feed.cpp
+++ src/dlss5-feed.cpp
@@ -66,7 +66,7 @@
 #include "feed_fsr1.h" // AMD FSR 1 EASU + RCAS: the optional expand-back for work_resolution < 100%
 #include "feed_pq12.h" // the D3D12 PQ<->linear pass, for the transports with no shaders of their own
 
-#define FEED_VERSION "1.17.0-beta.1"
+#define FEED_VERSION "1.17.0-beta.2"
 #ifndef FEED_BUILD_ID
 #define FEED_BUILD_ID "unknown"
 #endif
